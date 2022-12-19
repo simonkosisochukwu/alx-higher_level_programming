26b0eff1ee57c4cfc2ec90e26284b8000d410b5c
simonkosisochukwu <49375934+simonkosisochukwu@users.noreply.github.com>
2022-12-19 18:45:07 +0000

    update

---
+++ 0x05-python-exceptions/main.h
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+#ifndef MAIN_H
+#define MAIN_H
+
+def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
+def safe_print_division(a, b):
+def list_division(my_list_1, my_list_2, list_length):
+def raise_exception():
+def raise_exception_msg(message=""):
+def safe_print_integer_err(value):
+def safe_function(fct, *args):
+def magic_calculation(a, b):
+void print_python_list(PyObject *p);
+
+#endif /* MAIN_H */
